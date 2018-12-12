78b2a1d409848b6784b9800767efa96066121fa6
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2018-12-12 10:56:21 -0200

    Update Questão2.py

--- Questão2.py
+++ Questão2.py
@@ -5,7 +5,7 @@ class Queue:
   def enqueue(self,item):
     self.queue.append(item)
 
-  def desenqueue(self):
+  def dequeue(self):
     if not (self.isEmpty()):
       self.queue.pop(0)
   def isEmpty(self):
