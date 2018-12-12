e4e0c9d6a795f0cf4bd87c98989dc074814be668
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2018-12-12 10:47:58 -0200

    Questão 2 do Exercício de Fila

---
+++ Questão2.py
@@ -0,0 +1,15 @@
+class Queue:
+  def __init__(self):
+    self.queue = []
+  
+  def enqueue(self,item):
+    self.queue.append(item)
+
+  def desenqueue(self):
+    if not (isEmpty()):
+      self.queue.pop(0)
+  def isEmpty(self):
+    return len(self.queue) == 0
+  
+  def lenght(self):
+    return len(self.queue)
